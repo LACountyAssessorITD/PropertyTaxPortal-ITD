3f718fe90f097b3f54663d78d300d390da34bac2
Daniel Spencer <DSpencer@assessor.lacounty.gov>
2019-02-05 09:08:04 -0800

    controller added

--- src/PropertyTaxPortal/Controllers/HomeController.cs
+++ src/PropertyTaxPortal/Controllers/HomeController.cs
@@ -33,6 +33,13 @@ namespace PropertyTaxPortal.Controllers
         {
             return View();
         }
+        
+        public IActionResult PublicInquiry()
+        {
+            return View();
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
