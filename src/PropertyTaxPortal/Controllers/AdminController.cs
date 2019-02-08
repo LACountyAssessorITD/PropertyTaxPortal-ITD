ca4b5c245305c3d3fe8aaa047e55fb0e5dede48f
pattabi1 <pthumati@assessor.lacounty.gov>
2019-02-08 13:59:39 -0800

    test3

--- src/PropertyTaxPortal/Controllers/AdminController.cs
+++ src/PropertyTaxPortal/Controllers/AdminController.cs
@@ -10,7 +10,7 @@ namespace PropertyTaxPortal.Controllers
     {
         public IActionResult Index()
         {
-            int i = 0;
+            
             return View();
         }
 
--- src/PropertyTaxPortal/Controllers/HomeController.cs
+++ src/PropertyTaxPortal/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace PropertyTaxPortal.Controllers
 {
     public class HomeController : Controller
     {
-        int i = 10;
+        
         private readonly Email _email;
         private readonly IHostingEnvironment _host;
 
