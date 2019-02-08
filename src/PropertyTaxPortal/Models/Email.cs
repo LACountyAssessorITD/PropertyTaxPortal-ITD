27b3b19edf8e486331a481c67ce01db9d1032d7b
willielam <44653452+willielam@users.noreply.github.com>
2019-02-08 09:59:21 -0800

    wlam

---
+++ src/PropertyTaxPortal/Models/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyTaxPortal.Models
+{
+    public class Email
+    { 
+        public string host { get; set; }
+        public int port { get; set; }
+        public bool enableSsl { get; set; }
+        public string from { get; set; }
+        public string subject { get; set; }
+        public bool isBodyHtml { get; set; }
+
+    }
+}
