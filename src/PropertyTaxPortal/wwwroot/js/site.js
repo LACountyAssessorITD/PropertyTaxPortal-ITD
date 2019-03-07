bb8af4bbaf7fbafd1610e11fcd8ebb5b9c22fd4c
Daniel <dspencer@assessor.lacounty.gov>
2019-03-07 15:30:58 -0800

    Updated index.cshtml

--- src/PropertyTaxPortal/wwwroot/js/site.js
+++ src/PropertyTaxPortal/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).ready(function () {
+
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+            offset: '50%'
+        });
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+            offset: '50%'
+        });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp32').addClass('animated bounceInLeft');
+    }, {
+            offset: '50%'
+        });
+});
