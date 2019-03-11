776bfa1516ab74963a778e7adad7c19329db13d3
Daniel <dspencer@assessor.lacounty.gov>
2019-03-11 15:26:39 -0700

    Update Index.cshtml- Added features to Welcome Banner

--- src/PropertyTaxPortal/wwwroot/js/site.js
+++ src/PropertyTaxPortal/wwwroot/js/site.js
@@ -1,50 +1,65 @@
-﻿
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
 
     $('.js--wp-1').waypoint(function (direction) {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
-            offset: '50%'
-        });
+        offset: '50%'
+    });
 
     $('.js--wp-2').waypoint(function (direction) {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
-            offset: '50%'
-        });
+        offset: '50%'
+    });
 
     $('.js--wp-3').waypoint(function (direction) {
-        $('.js--wp32').addClass('animated bounceInLeft');
+        $('.js--wp-3').addClass('animated bounceInLeft');
     }, {
-            offset: '50%'
-        });
+        offset: '50%'
+    });
+    
+    $('.js--wp-8').waypoint(function (direction) {
+        $('.js--wp-8').addClass('animated fadeInLeft');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-9').waypoint(function (direction) {
+        $('.js--wp-9').addClass('animated fadeInRight');
+    }, {
+        offset: '50%'
+    });
+
     $('.js--wp-4').hover(function () {
-        $('.js--wp-4').toggleClass('animated pulse');
-        $('.c-1').toggleClass('animated flip')
-    },
-        {
+            $('.js--wp-4').toggleClass('animated pulse')
+        },
+        function () {
+            $('.c-1').toggleClass('animated flip')
+        }, {
             offset: '50%'
         });
     $('.js--wp-5').hover(function () {
-        $('.js--wp-5').toggleClass('animated pulse');
-        $('.c-2').toggleClass('animated flip')
-    },
-        {
+            $('.js--wp-5').toggleClass('animated pulse')
+        },
+        function () {
+            $('.c-2').toggleClass('animated flip')
+        }, {
             offset: '50%'
         });
     $('.js--wp-6').hover(function () {
-        $('.js--wp-6').toggleClass('animated pulse');
-        $('.c-3').toggleClass('animated flip')
-    },
-        {
+            $('.js--wp-6').toggleClass('animated pulse')
+        },
+        function () {
+            $('.c-3').toggleClass('animated flip')
+        }, {
             offset: '50%'
         });
     $('.js--wp-7').hover(function () {
-        $('.js--wp-7').toggleClass('animated pulse');
-        $('.c-4').toggleClass('animated flip')
-    },
-        {
+            $('.js--wp-7').toggleClass('animated pulse')
+        },
+        function () {
+            $('.c-4').toggleClass('animated flip')
+        }, {
             offset: '50%'
         });
+    
 });
