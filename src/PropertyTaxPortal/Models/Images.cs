6cd59b088d767517c8e744d3406961996864cd4e
willielam <44653452+willielam@users.noreply.github.com>
2019-03-18 15:30:05 -0700

    wlam

---
+++ src/PropertyTaxPortal/Models/Images.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyTaxPortal.Models
+{
+    public class Images
+    {
+        public int ID { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
