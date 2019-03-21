cc3d667198aec6e868215b367553a70b62143cbe
willielam <44653452+willielam@users.noreply.github.com>
2019-03-21 15:35:08 -0700

    wlam

--- src/PropertyTaxPortal/Startup.cs
+++ src/PropertyTaxPortal/Startup.cs
@@ -38,7 +38,7 @@ namespace PropertyTaxPortal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+
             var email = Configuration.GetSection("Email");
             services.Configure<Email>(email);
 
