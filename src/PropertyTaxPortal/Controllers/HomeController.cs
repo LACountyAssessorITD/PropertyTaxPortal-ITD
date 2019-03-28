033be0cc670b3268305a73aadc7c4002c39a908d
willielam <44653452+willielam@users.noreply.github.com>
2019-03-27 22:17:11 -0700

    wlam

--- src/PropertyTaxPortal/Controllers/HomeController.cs
+++ src/PropertyTaxPortal/Controllers/HomeController.cs
@@ -80,7 +80,7 @@ namespace PropertyTaxPortal.Controllers
             if (ModelState.IsValid)
             {
                 //Find the responsible department and address
-                string sSubjectEnghlish = "";
+                string sSubjectEnglish = "";
                 string sEmail = "";
                 string sEmailDept = "";
                 List<EmailTrackingCount> lEmailTracking = new List<EmailTrackingCount>();
@@ -92,9 +92,8 @@ namespace PropertyTaxPortal.Controllers
                 lEmailTracking = _context.emailTrackingCount.FromSql("PTP_emailTrackingIncrement").ToList();
                 sEmailTrackingCount = lEmailTracking.First().emailTrackingCount.ToString();
                 model.emailTrackingCount = sEmailTrackingCount;
-                //sEmailDept = model.subjectValue.Substring(model.subjectValue.IndexOf("@"), model.subjectValue.Length - model.subjectValue.IndexOf("@"));
-                sSubjectEnghlish = model.subjectValue.Substring(0, model.subjectValue.IndexOf("|")); // Assessment Appeals
-                model.subjectEnglish = sSubjectEnghlish; // Assessment Appeals
+                sSubjectEnglish = model.subjectValue.Substring(0, model.subjectValue.IndexOf("|")); // Assessment Appeals
+                model.subjectEnglish = sSubjectEnglish; // Assessment Appeals
                 sEmail = model.subjectValue.Substring(model.subjectValue.IndexOf("|")+1, model.subjectValue.Length - model.subjectValue.IndexOf("|")-1); // aaboffice@bos.lacounty.gov
                 sEmailDept = model.subjectValue.Substring(model.subjectValue.IndexOf("@"), model.subjectValue.Length - model.subjectValue.IndexOf("@")); // "@bos.lacounty.gov
                 switch (sEmailDept)
@@ -159,7 +158,6 @@ namespace PropertyTaxPortal.Controllers
                 MailAddress to;
                 if (_host.IsProduction())
                 {
-                    //to = new MailAddress(model.subjectValue);
                     to = new MailAddress(sEmail);
                 }
                 else
@@ -168,9 +166,7 @@ namespace PropertyTaxPortal.Controllers
                 }
 
                 MailMessage mail = new MailMessage(from, to);
-                //string strSubjectText = model.subjectText;
-                //string strSubjectText = sSubjectEnghlish;
-                string sSubject = sSubjectEnghlish + " - " + _email.subject + " - Reference # " + sEmailTrackingCount;
+                string sSubject = sSubjectEnglish + " - " + _email.subject + " - Reference # " + sEmailTrackingCount;
 
                 mail.Subject = sSubject;
                 mail.IsBodyHtml = _email.isBodyHtml;
@@ -178,7 +174,7 @@ namespace PropertyTaxPortal.Controllers
                 MailAddress bcc = new MailAddress(_email.bcc);
                 mail.Bcc.Add(bcc);
 
-                mail.Body = BuildMailBody("/Templates/EmailTemplate/PublicInquiry.html", model);
+                mail.Body = BuildMailBody("/Templates/EmailTemplate/PublicInquiry.html", model, 0);
                 try
                 {
                     SmtpClient smtp = new SmtpClient();
@@ -191,10 +187,11 @@ namespace PropertyTaxPortal.Controllers
                         from = new MailAddress(_email.from);
                         to = new MailAddress(model.emailAddr.Trim());
                         mail = new MailMessage(from, to);
+                        sSubject = _localizer[sSubjectEnglish] + " - " + _localizer[_email.subject] + " - " + _localizer["Reference"] + " # " + sEmailTrackingCount;
                         mail.Subject = sSubject;
                         mail.IsBodyHtml = _email.isBodyHtml;
                         mail.Priority = MailPriority.Normal;
-                        mail.Body = BuildMailBody("/Templates/EmailTemplate/PublicInquiryUser.html", model);
+                        mail.Body = BuildMailBody("/Templates/EmailTemplate/PublicInquiryUser.html", model, 1);
 
                         smtp.Send(mail); //email to user
                     }
@@ -219,8 +216,9 @@ namespace PropertyTaxPortal.Controllers
         /// </summary>
         /// <param name="path"></param>
         /// <param name="model"></param>
+        /// <param name="iUserEmail"></param>
         /// <returns></returns>
-        public string BuildMailBody (string path, PublicInquiryViewModel model)
+        public string BuildMailBody (string path, PublicInquiryViewModel model, int iUserEmail)
         {
             var wwwRoot = _host.WebRootPath;
             string sOriginalBody = "";
@@ -229,8 +227,31 @@ namespace PropertyTaxPortal.Controllers
             {
                 sOriginalBody = reader.ReadToEnd();
 
-                sOriginalBody = sOriginalBody.Replace("{Subject}", model.subjectEnglish);
-                sOriginalBody = sOriginalBody.Replace("{Department}", string.IsNullOrEmpty(model.Department) ? "" : model.Department);
+                if (iUserEmail == 1)
+                {
+                    sOriginalBody = sOriginalBody.Replace("{PublicInquiryEmailLabel}", _localizer["Public Inquiry Email"]);
+                    sOriginalBody = sOriginalBody.Replace("{NotReplyLabel}", _localizer["Please do NOT reply to this email"]);
+                    sOriginalBody = sOriginalBody.Replace("{ResponsibleDepartment}", _localizer[model.responsibleDepartment]);
+                    sOriginalBody = sOriginalBody.Replace("{ReferenceNumberLabel}", _localizer["The Reference number for your inquiry is"]);
+                    sOriginalBody = sOriginalBody.Replace("{NamerLabel}", _localizer["Name"]);
+                    sOriginalBody = sOriginalBody.Replace("{BusinessNameLabel}", _localizer["Business Name"]);
+                    sOriginalBody = sOriginalBody.Replace("{MailingAddressLabel}", _localizer["Mailing Address"]);
+                    sOriginalBody = sOriginalBody.Replace("{EmailLabel}", _localizer["Email"]);
+                    sOriginalBody = sOriginalBody.Replace("{DaytimePhoneNumberLabel}", _localizer["Daytime Phone Number"]);
+                    sOriginalBody = sOriginalBody.Replace("{FaxNumberLabel}", _localizer["Fax Number"]);
+                    sOriginalBody = sOriginalBody.Replace("{PropertyAddressLabel}", _localizer["Property Address"]);
+                    sOriginalBody = sOriginalBody.Replace("{AINLabel}", _localizer["Assessor Identification Number"]);
+                    sOriginalBody = sOriginalBody.Replace("{CompanyNumberLabel}", _localizer["Company Number"]);
+                    sOriginalBody = sOriginalBody.Replace("{RoutingIndexLabel}", _localizer["Routing Index"]);
+                    sOriginalBody = sOriginalBody.Replace("{CommentsLabel}", _localizer["Comments"]);
+                    sOriginalBody = sOriginalBody.Replace("{SubmitLabel}", _localizer["To submit your inquiries, please use our Inquiry Form at"]);
+                }
+                else
+                {
+                    sOriginalBody = sOriginalBody.Replace("{Subject}", model.subjectEnglish);
+                    sOriginalBody = sOriginalBody.Replace("{Department}", string.IsNullOrEmpty(model.Department) ? "" : model.Department);
+                }
+  
                 sOriginalBody = sOriginalBody.Replace("{LastName}", model.lastName);
                 sOriginalBody = sOriginalBody.Replace("{FirstName}", model.firstName);
                 sOriginalBody = sOriginalBody.Replace("{BusinessName}", string.IsNullOrEmpty(model.businessName) ? "" : model.businessName);
