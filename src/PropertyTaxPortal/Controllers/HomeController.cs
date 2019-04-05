4134233e896ea672a22474c341c3b16e6b3351ca
Daniel <dspencer@assessor.lacounty.gov>
2019-04-05 15:45:53 -0700

    Now Correctly check EndOn Date

--- src/PropertyTaxPortal/Controllers/HomeController.cs
+++ src/PropertyTaxPortal/Controllers/HomeController.cs
@@ -39,7 +39,7 @@ namespace PropertyTaxPortal.Controllers
 
         public IActionResult Index()
         {
-            var context = _context.News.Where(b => b.Active == 1).OrderBy(b => b.SOrder).ToList();
+            var context = _context.News.Where(b => (b.Active == 1 && DateTime.Compare(b.EndOn, DateTime.Now) > 0)).OrderBy(b => b.SOrder).ToList();
             return View(context);
         }
 
@@ -62,7 +62,7 @@ namespace PropertyTaxPortal.Controllers
         }
         public IActionResult News()
         {
-            var context = _context.News.Where(b => b.Active == 1).OrderBy(b => b.SOrder).ToList();
+            var context = _context.News.Where(b => (b.Active == 1 && DateTime.Compare(b.EndOn, DateTime.Now) > 0)).OrderBy(b => b.SOrder).ToList();
             return View(context);
         }
 
