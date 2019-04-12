f14d04088155f5963003ed3665988fd0bf3ee0a0
Pattabi Thumati <pthumati@assessor.lacounty.gov>
2019-04-12 15:26:16 -0700

    code cleanup

--- src/PropertyTaxPortal/Controllers/FAQsController.cs
+++ src/PropertyTaxPortal/Controllers/FAQsController.cs
@@ -22,33 +22,21 @@ namespace PropertyTaxPortal.Controllers
             _context = context;
         }
 
-        // GET: FAQs
-        
+        // GET: FAQs        
         public async Task<IActionResult> Index(int page = 1)
         {
             var query = _context.faq.FromSql("PTP_getAllFAQs").OrderBy(f => f.sOrder);
             var model = await PagingList.CreateAsync(query, 10, page);
             return View(model);
-        }
+        }       
 
-        //// GET: FAQs
-        //[HttpGet]
-        //public IActionResult View()
-        //{
-
-        //    //var query = _context.faq.AsNoTracking().OrderBy(f => f.sOrder);
-        //    //var model =  PagingList.CreateAsync(query, 3, page);
-        //    var model = _context.faq.AsNoTracking().OrderBy(f => f.sOrder);
-        //    return PartialView("View", model);
-        //}
-
-
-        // GET: FAQs/Create
-       
+        // GET: FAQs/Create       
         public IActionResult CreateorEdit(int id = 0)
         {
             if (id == 0)
             {
+                ViewBag.Caption = "Create FAQ";
+                ViewBag.ButtonValue = "Create";
                 LoadFAQCategoryTypes();
                 return View(new FAQ());
             }
@@ -57,7 +45,6 @@ namespace PropertyTaxPortal.Controllers
                 List<Category> listCatcodes = new List<Category>();
                 listCatcodes = _context.category.Where(c => c.categoryType == "FQC").ToList();
                 List<SelectListItem> li = new List<SelectListItem>();
-                //var query = _context.Database.ExecuteSqlCommand("PTP_getAllFAQsbyCategoryID @p0", fa);
                 var query = _context.faq.FromSql("PTP_getAllFAQsbyCategoryID @p0", id).OrderBy(f => f.sOrder);
                 string strCatID = query.SingleOrDefault().CategoryID.ToString();
                 foreach (var lcat in listCatcodes)
@@ -71,67 +58,13 @@ namespace PropertyTaxPortal.Controllers
                         li.Add(new SelectListItem { Text = lcat.Descr, Value = Convert.ToString(lcat.CategoryID) });
                     }
                 }
+                ViewBag.Caption = "Update FAQ";
+                ViewBag.ButtonValue = "Edit";
                 ViewBag.CategoryID = li;
                 return View(_context.faq.Find(id));
+
             }
         }
-        
-        public IActionResult LoadFAQCategoryTypes()
-        {
-            List<Category> listCatcodes = new List<Category>();
-            //var query = _context.Database.ExecuteSqlCommand("PTP_getCategoriesForFAQ");
-
-            listCatcodes=_context.category.Where(c => c.categoryType == "FQC").ToList();
-            List<SelectListItem> li = new List<SelectListItem>();
-            foreach (var lcat in listCatcodes)
-            {
-                li.Add(new SelectListItem { Text = lcat.Descr, Value =Convert.ToString(lcat.CategoryID) });
-            }
-            ViewBag.CategoryID = li;
-            return View();
-        }
-
-        //public IActionResult LoadFAQFeaturedCode()
-        //{
-        //    List<FAQ> listFeaturedcodes = new List<FAQ>();
-        //    //var query = _context.Database.ExecuteSqlCommand("PTP_getCategoriesForFAQ");
-
-        //    listFeaturedcodes = _context.faq.Where(c => c.categoryType == "FQC").ToList();
-        //    List<SelectListItem> li = new List<SelectListItem>();
-        //    foreach (var lcat in listCatcodes)
-        //    {
-        //        li.Add(new SelectListItem { Text = lcat.Descr, Value = Convert.ToString(lcat.CategoryID) });
-        //    }
-        //    ViewBag.CategoryID = li;
-        //    return View();
-        //}
-
-        //public IActionResult AllFAQs()
-        //{
-        //    dynamic FaqFinalModel = new ExpandoObject();
-        //    var modelTaxBillFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f=>f.CategoryID==1).OrderBy(f => f.sOrder);
-        //    var modelRefundFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f => f.CategoryID == 2).OrderBy(f => f.sOrder);
-        //    FaqFinalModel.TaxBill = modelTaxBillFAQs;
-        //    FaqFinalModel.Refund = modelRefundFAQs;
-        //    return View(FaqFinalModel);
-        //}
-
-        
-
-        public IActionResult FAQTabs()
-        {
-            dynamic FaqFinalModel = new ExpandoObject();
-            var modelTaxBillFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f => f.CategoryID == 1).OrderBy(f => f.sOrder);
-            var modelRefundFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f => f.CategoryID == 2).OrderBy(f => f.sOrder);
-            var modelPropertyFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f => f.CategoryID == 3).OrderBy(f => f.sOrder);
-            var modelOwnershipFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f => f.CategoryID == 4).OrderBy(f => f.sOrder);
-            FaqFinalModel.TaxBill = modelTaxBillFAQs;
-            FaqFinalModel.Refund = modelRefundFAQs;
-            FaqFinalModel.Property = modelPropertyFAQs;
-            FaqFinalModel.Ownership = modelOwnershipFAQs;
-            return View(FaqFinalModel);
-        }
-
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -147,13 +80,57 @@ namespace PropertyTaxPortal.Controllers
                 {
                     int faqResults = _context.Database.ExecuteSqlCommand("PTP_updateFAQ @p0,@p1,@p2,@p3,@p4,@p5,@p6", fAQ.FAQID, fAQ.webSectionID, fAQ.CategoryID, fAQ.question, fAQ.answer, fAQ.updatedOn, fAQ.featuredCode);
                 }
-                return RedirectToAction(nameof(Index)); 
+                return RedirectToAction(nameof(Index));
             }
             return View(fAQ);
         }
 
-        // GET: FAQs/Edit/5
-       
+        public ActionResult UpArrow(int? id, int? pagenum)
+        {
+            if (id > 0)
+            {
+                int faqResults = _context.Database.ExecuteSqlCommand("PTP_upArrowSwapping @p0", id);
+            }
+            return RedirectToAction(nameof(Index), new { page = pagenum });
+        }
+
+        public ActionResult DownArrow(int? id, int? pagenum)
+        {
+            if (id > 0)
+            {
+                int faqResults = _context.Database.ExecuteSqlCommand("PTP_downArrowSwapping @p0", id);
+            }
+            return RedirectToAction(nameof(Index), new { page = pagenum });
+        }
+
+        public IActionResult LoadFAQCategoryTypes()
+        {
+            List<Category> listCatcodes = new List<Category>();
+            listCatcodes=_context.category.Where(c => c.categoryType == "FQC").ToList();
+            List<SelectListItem> li = new List<SelectListItem>();
+            foreach (var lcat in listCatcodes)
+            {
+                li.Add(new SelectListItem { Text = lcat.Descr, Value =Convert.ToString(lcat.CategoryID) });
+            }
+            ViewBag.CategoryID = li;
+            return View();
+        }              
+
+        public IActionResult FAQTabs()
+        {
+            dynamic FaqFinalModel = new ExpandoObject();
+            var modelTaxBillFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f => f.CategoryID == 1).OrderBy(f => f.sOrder);
+            var modelRefundFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f => f.CategoryID == 2).OrderBy(f => f.sOrder);
+            var modelPropertyFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f => f.CategoryID == 3).OrderBy(f => f.sOrder);
+            var modelOwnershipFAQs = _context.faq.FromSql("PTP_getAllFAQs").Where(f => f.CategoryID == 4).OrderBy(f => f.sOrder);
+            FaqFinalModel.TaxBill = modelTaxBillFAQs;
+            FaqFinalModel.Refund = modelRefundFAQs;
+            FaqFinalModel.Property = modelPropertyFAQs;
+            FaqFinalModel.Ownership = modelOwnershipFAQs;
+            return View(FaqFinalModel);
+        }
+
+        // GET: FAQs/Edit/5       
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -167,28 +144,6 @@ namespace PropertyTaxPortal.Controllers
                 return NotFound();
             }
             return View(fAQ);
-        }
-
-        
-        [HttpPost]
-        public ActionResult UpArrow(int? id)
-        {
-            if (id > 0)
-            {
-                int faqResults = _context.Database.ExecuteSqlCommand("PTP_upArrowSwapping @p0", id);
-            }
-            return RedirectToAction(nameof(Index));
-        }
-
-        [HttpPost]
-        public ActionResult DownArrow(int? id)
-        {
-            if (id > 0)
-            {
-                int faqResults = _context.Database.ExecuteSqlCommand("PTP_downArrowSwapping @p0", id);
-            }
-            //int pageNumber = Convert.ToInt16(HttpContext.Request.Query["page"]);
-            return RedirectToAction(nameof(Index));
         }        
 
         [HttpGet]
